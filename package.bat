730102b0da9aaf7240f46b5a0eb2b0fc9b7efa1a
Edward Yang <qwbarch@gmail.com>
2023-12-28 04:10:09 -0500

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,3 +1,3 @@
 @echo off
 call ./build.bat
-powershell Compress-Archive -Path "bin/Release/AutoStart.dll", "manifest.json", "icon.png", "README.md" -DestinationPath "package.zip"
+powershell Compress-Archive -Force -Path "bin/Release/AutoStart.dll", "manifest.json", "icon.png", "README.md" -DestinationPath "package.zip"
