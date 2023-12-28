f9470f25401242affba36166d0b7bb144c4a58d1
Edward Yang <qwbarch@gmail.com>
2023-12-28 01:52:27 -0500

    Add NetworkValidator

--- Core/NetworkValidator.cs
+++ Core/NetworkValidator.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Sockets;
 
 namespace AutoStart.Core
@@ -9,9 +10,53 @@ namespace AutoStart.Core
   /// </summary>
   class NetworkValidator
   {
-    public static bool ServerIsRunning()
+    private readonly Config Config;
+
+    public NetworkValidator(Config config)
     {
-      return false;
+      Config = config;
+    }
+
+    /// <summary>
+    /// Check whether the network validator server is running.
+    /// If it's on, it indicates that we should join the LAN game instead of hosting one.
+    /// </summary>
+    public bool ServerIsRunning()
+    {
+      using (var client = new TcpClient())
+      {
+        try
+        {
+          client.Connect(
+            Config.ValidatorServerHost.Value,
+            Config.ValidatorServerPort.Value
+          );
+          client.Close();
+          return true;
+        }
+        catch (SocketException)
+        {
+          return false;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Hosts the network validator server.
+    /// Note: This is a blocking function, and needs to be run on a separate thread.
+    /// </summary>
+    public void StartServer()
+    {
+      var server = new TcpListener(IPAddress.Parse(Config.ValidatorServerHost.Value), Config.ValidatorServerPort.Value);
+      server.Start();
+      while (true)
+      {
+        try
+        {
+          server.AcceptTcpClient().Close();
+        }
+        catch (SocketException) { }
+      }
     }
   }
 }
