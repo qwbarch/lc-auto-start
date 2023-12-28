c7930dc32b19d7c289fa0079bb3c6bfb78c9f01c
Edward Yang <qwbarch@gmail.com>
2023-12-28 01:15:49 -0500

    Create base plugin

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AutoStart
+{
+  [BepInPlugin(PluginId, PluginName, PluginVersion)]
+  public class Plugin : BaseUnityPlugin
+  {
+    public const string PluginId = "qwbarch.AutoStart";
+    public const string PluginName = "AutoStart";
+    public const string PluginVersion = "1.0.0";
+
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+      Logger = BepInEx.Logging.Logger.CreateLogSource(PluginId);
+      //new Harmony(PluginId).PatchAll(typeof());
+    }
+  }
+}
